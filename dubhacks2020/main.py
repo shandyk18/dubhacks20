84d8131bfc8568ee92ce9d2e3a695dee3d204f34
yehric2018 <eric.yehj@outlook.com>
2020-10-17 16:07:17 -0700

    firebase

--- dubhacks2020/main.py
+++ dubhacks2020/main.py
@@ -1,9 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from firebase_admin import credentials, firestore, initialize_app
+
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+cred = credentials.Certificate("firebasekey.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+survey_ref = db.collection('surveyID')
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+	return "Hello world"
+
+@app.route('/test')
+def test_get():
+	try:
+		survey = survey_ref.document("123").get()
+		return jsonify(survey.to_dict()), 200
+	except Exception as e:
+		return f"An Error Occured: {e}"
 
 if __name__ == '__main__':
 	app.run()
