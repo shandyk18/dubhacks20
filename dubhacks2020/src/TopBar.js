bb40d108d90a69dbfe806c2ffd49da840f79f16f
Julia Kim <jkim0036@gmail.com>
2020-10-17 17:34:19 -0700

    factor out top bar

---
+++ dubhacks2020/src/TopBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+
+const divStyle = {
+  backgroundColor: '#7CD7D1',
+  width: '100%',
+  paddingTop: '15px',
+  paddingLeft: '5px',
+  paddingBottom: '15px'
+};
+
+const Top = () => (
+  <div style={divStyle}>
+    <Box>inclusivity indicator</Box>
+  </div>
+);
+
+export default Top;
--- dubhacks2020/src/surveyPage/SurveyPage.js
+++ dubhacks2020/src/surveyPage/SurveyPage.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import './SurveyPage.css';
 import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
@@ -9,8 +7,8 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Rating from '@material-ui/lab/Rating';
 import TextField from '@material-ui/core/TextField';
-
-
+import Button from '@material-ui/core/Button';
+import Top from '../TopBar';
 
 
 class SurveyPage extends React.Component {
@@ -30,24 +28,22 @@ class SurveyPage extends React.Component {
     }
 
     handleChange1 = (event) => {
-        console.log(this.state.answer1);
-
         this.setState({ answer1: event.target.value });
-        console.log(this.state.answer1);
     };
 
     handleChange2 = (event) => {
-        console.log(this.state.answer2);
-        console.log(event.target.value);
-
         this.setState({ answer2: event.target.value });
+    };
+
+    handleCommentChange = (event) => {
+        this.setState({ comments: event.target.value })
     };
 
     render() {
         return (
             <div>
-                <Box className="surveyTop">inclusivity indicator</Box>
-                <Box className="surveyContent">
+                <Top />
+                <Box paddingTop="15px" paddingLeft="15px">
                     <Box fontWeight="fontWeightBold" fontSize={40}>
                         Title FirstName LastName
                    </Box>
@@ -75,7 +71,12 @@ class SurveyPage extends React.Component {
                         What are your overall feelings of this class's inclusivity policies?
                     </Box>
                     <Box component="fieldset" mb={3} borderColor="transparent">
-                        <Rating name="customized-10" defaultValue={2} max={5} size="large" />
+                        <Rating
+                            name="customized-10"
+                            defaultValue={2}
+                            max={5}
+                            size="large"
+                            onChange={this.handleChange2} />
                     </Box>
 
                     <Box fontWeight="fontWeightBold" paddingBottom="10px">
@@ -88,10 +89,19 @@ class SurveyPage extends React.Component {
                         type="comments"
                         autoComplete="current-comment"
                         variant="outlined"
-                        style = {{width: 600}}
+                        style={{ width: 600 }}
+                        onChange={this.handleCommentChange}
                     />
                 </Box>
-
+                <Box paddingLeft="15px" paddingTop="15px">
+                    <Button
+                        style={{backgroundColor:"#7CD7D1"}}
+                        variant="outlined"
+                        onClick={() => { console.log(this.state.answer1 + " " + this.state.answer2 + " " + this.state.comments) }}
+                    >
+                        Submit
+                </Button>
+                </Box>
             </div>
         )
     }
