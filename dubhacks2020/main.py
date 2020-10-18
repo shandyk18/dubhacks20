e4486b04f9fef83ead1d985d2d4b5c93f5267932
jeremychen99 <jchen99@uw.edu>
2020-10-17 21:45:30 -0700

    grabbing responses from database

--- dubhacks2020/main.py
+++ dubhacks2020/main.py
@@ -12,8 +12,56 @@ survey_ref = db.collection('surveyID')
 def get_html():
 	return app.send_static_file('index.html')
 
-#@app.route('/getAnswer1')
-#def getOne():
+@app.route('/getAnswer1')
+def getOne():
+	surveyID = request.json["surveyID"]
+	result = {
+		"yes": 0,
+		"no": 0
+	}
+
+
+
+	for survey in survey_ref:
+		docs = survey_ref.document(surveyID).collection('responses').stream()
+		for doc in docs:
+			res = doc.to_dict()
+			if (res["q1"]):
+				result["yes"] += 1
+			else:
+				result["no"] += 1
+	return jsonify(result), 200
+	# survey = survey_ref.document('123').collection('responses').document('n8xs9YiTtLxou9pdC3Li').get()
+	# return jsonify(survey.to_dict()), 200
+
+@app.route('/getAnswer2')
+def getTwo():
+	result = {
+		"1" : 0,
+		"2" : 0,
+		"3" : 0,
+		"4" : 0,
+		"5" : 0
+	}
+
+	docs = survey_ref.document('123').collection('responses').stream()
+	
+	for doc in docs:
+		res = doc.to_dict()
+		
+	return jsonify(result), 200
+
+@app.route('/comments')
+def get_comments():
+	result = {}
+	index = 0
+	docs = survey_ref.document('123').collection('responses').stream()
+	for doc in docs:
+		actual_document = doc.to_dict()
+		result[index] = actual_document['comments']
+		index += 1
+	return jsonify(result), 200
+
 
 @app.route('/test')
 def test_get(): 
@@ -39,7 +87,8 @@ def add_response():
 			"q2": q2,
 			"comment": comment,
 		}
-		response_ref = survey_ref.document(surveyID).collection('reponses').document('18').set(response)
+		id = survey_ref.document(surveyID).collection('reponses').document().getId()
+		response_ref = survey_ref.document(surveyID).collection('reponses').document(id).set(response)
 		return jsonify({"success": True}), 200
 	except Exception as e:
 		return f"An Error Occured: {e}"
