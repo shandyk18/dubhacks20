34c893b7daef60d839ef1751e8a26088bd98c13b
yehric2018 <eric.yehj@outlook.com>
2020-10-17 20:21:39 -0700

    route for adding responses

--- dubhacks2020/main.py
+++ dubhacks2020/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from firebase_admin import credentials, firestore, initialize_app
+import json
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 cred = credentials.Certificate("firebasekey.json")
@@ -11,9 +12,8 @@ survey_ref = db.collection('surveyID')
 def get_html():
 	return app.send_static_file('index.html')
 
-@app.route('/getAnswer1')
-def getOne():
-	
+#@app.route('/getAnswer1')
+#def getOne():
 
 @app.route('/test')
 def test_get(): 
@@ -30,21 +30,19 @@ def add_response():
 		q1 = request.json["q1"]
 		q2 = request.json["q2"]
 		comment = request.json["comment"]
-		timestamp = request.json["timestamp"]
 
-		if (not q1 or not q2 or not timestamp or not surveyID):
+		if (not q1 or not q2 or not surveyID):
 			return {"error": "one or more attributes missing"}
 
 		response = {
 			"q1": q1,
 			"q2": q2,
 			"comment": comment,
-			"timestamp": timestamp,
 		}
-		response_ref = survey_ref.document(surveyID).collection('reponses').set(response)
-        return jsonify({"success": True}), 200
-
-# @app.route('/')
+		response_ref = survey_ref.document(surveyID).collection('reponses').document('18').set(response)
+		return jsonify({"success": True}), 200
+	except Exception as e:
+		return f"An Error Occured: {e}"
 
 if __name__ == '__main__':
 	app.run()
